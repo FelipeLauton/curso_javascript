c28a2dded7a04c3d04fd4af0eaec0e1c1e776d81
Felipe Lauton <felipelauton021@gmail.com>
2021-10-15 14:54:26 -0300

    Introdução ao JavaScript

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var nome = "Felipe Lauton";
+var idade = 20;
+var idade2 = 10;
+//alert(nome + " tem " + idade + " anos.");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
