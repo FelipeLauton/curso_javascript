7992fd8060a0c7dcb4212cf1a361d2c745729b6a
Felipe Lauton <felipelauton021@gmail.com>
2021-10-15 17:32:10 -0300

    Aula sobre Array

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-var nome = "Felipe Lauton";
-var idade = 20;
-var idade2 = 10;
+var fruta = {nome : "maça", cor : "vermelha"};
+console.log(fruta);
+
+//var lista = ["maça", "banana", "uva"];
+//lista.push("laranja");
+//console.log(lista);
+//lista.pop();
+//console.log(lista.length);
+//console.log(lista.toString);
+//console.log(lista.join(" - "));
+
+
+//var nome = "Felipe Lauton";
+//var idade = 20;
+//var idade2 = 10;
 //alert(nome + " tem " + idade + " anos.");
 //alert(idade + idade2);
-console.log(nome);
-console.log(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
